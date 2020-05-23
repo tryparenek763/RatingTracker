38e5d93913469f6a3c22ae7fdd9a48e89747f55b
mtimoshenko <minzdrav1631@gmail.com>
2020-05-23 22:54:45 +0400

    wip

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,14 +1,40 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Form, Select, Button
 } from 'antd';
-import data from './form.json';
 import styles from './index.module.css';
 import Group from '../group';
 
 const { Option } = Select;
 function ApplicationForm() {
+  const [data, setData] = useState(
+    {
+      groups: [{
+        title: 'Нет данных',
+        subgroups: [{
+          title: 'Нет данных',
+          questions: [{
+            title: 'Нет данных',
+            description: 'Нет данных'
+          }]
+        }]
+      }
+      ]
+    }
+  );
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    fetch('/template', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+      .then(res => res.json())
+      .then(setData);
+  }, []);
   const handleSubmit = (...args) => {
     console.log(...args);
   };
